5e00a7db345c0cee5937ee5586bcf2469d840c52
Andrew Briley <andrewbriley01@gmail.com>
2019-07-09 13:24:11 +1000

    update file structure

--- core/client/App.js
+++ core/client/App.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
 import HomeDash from './home/HomeDash.js';
 import Landing from './landing/Landing.js';
-import User from './user/User.js';
-import NavMenu from './NavMenu'
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <NavMenu />
         {/* need to render components based on state */}
         <HomeDash />
-        <User />
         <Landing />
       </div>
     );
