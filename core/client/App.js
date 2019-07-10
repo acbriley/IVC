302736ee4ceab289feb600e21fc88c66456f544c
Andrew Briley <andrewbriley01@gmail.com>
2019-07-10 12:18:08 +1000

    update file structure

--- core/client/App.js
+++ core/client/App.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import HomeDash from './home/HomeDash.js';
 import Landing from './landing/Landing.js';
 import './App.css';
+import {Route, Link, BrowserRouter} from 'react-router-dom';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
         {/* need to render components based on state */}
-        <HomeDash />
-        <Landing />
+        <BrowserRouter>
+          <div>
+            <Link to="/home"></Link>
+            <Route path="/home" component={HomeDash}></Route>
+          </div>
+          <div>
+            <Link to="/"></Link>
+            <Route path="/" component={Landing}></Route>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
