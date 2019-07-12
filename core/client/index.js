00f28b09a1d1fd2ddb4747011569dd9effddf763
Andrew Briley <andrewbriley01@gmail.com>
2019-07-12 15:37:01 +1000

    add redeux setup

--- core/client/index.js
+++ core/client/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react';
 import './index.css';
 import App from './App';
-import BrowserRouter from 'react-router-dom'; 
-// import registerServiceWorker from './registerServiceWorker';
+import BrowserRouter from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk'; 
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-// registerServiceWorker();
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk),
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(<Provider store={store}> <BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
